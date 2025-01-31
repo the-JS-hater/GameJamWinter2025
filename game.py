c93f9c62809030b8d51b90e21de098384ec3b95c
Morgan <mnordberg01@gmail.com>
2025-01-31 21:26:40 +0100

    m

--- game.py
+++ game.py
@@ -10,8 +10,12 @@
     globimport("pyray"),
     globimport("itertools"),
     globimport("functools"),
+
+    # GUI constants
     window_w := 680,
     window_h := 420,
+    healthbar_w := 80,
+    healthbar_h := 15,
     
     # HELPER FUNCTIONS
     
@@ -22,6 +26,17 @@
             a['y'] + a['h'] <= b['y'],
             b['y'] + b['h'] <= a['y'],
             ])
+    ),
+
+    update_cooldowns := (
+        lambda player : (
+            player.update(
+                weapon_cooldown = player["weapon_cooldown"] - 1,
+            ) if player["weapon_cooldown"] > 0 else None,
+            player.update(
+                damage_cooldown = player["damage_cooldown"] - 1,
+            ) if player["damage_cooldown"] > 0 else None
+        )
     ),
 
     fire_pistol := (
@@ -39,7 +54,17 @@
     
     # Game state
     weapons := {"pistol": fire_pistol},
-    player := {"x": 200, "y": 180, "dx": 0, "dy": 1, "w": 10, "h": 20, "weapon": "pistol"},
+    weapon_cooldowns := {"pistol": 60},
+
+    player := {"x": 200, "y": 180, 
+               "dx": 0, "dy": 1, 
+               "w": 10, "h": 20, 
+               "weapon": "pistol",
+               "health": 1.0,
+               "damage_cooldown": 0,
+               "weapon_cooldown": 0,
+                },
+
     robot1 := {"x": 50, "y": 50, "w": 10, "h": 20},
     robot2 := {"x": 150, "y": 220, "w": 10, "h": 20},
     robots := [robot1, robot2],
@@ -71,21 +96,49 @@
             moved_player.__setitem__("dx", moved_player["x"] - player["x"]),
             moved_player.__setitem__("dy", moved_player["y"] - player["y"]),
             
+
             # Move player
             player.update(moved_player) if 
-                (moved_player["dx"] != 0 or moved_player["dy"] != 0) and
-                not any(
-                    has_collision(moved_player, entity)
-                for entity in robots) else None,
+                (moved_player["dx"] != 0 or moved_player["dy"] != 0) else None,
             
+            #    and
+            #    not any(
+            #        has_collision(moved_player, entity)
+            #    for entity in robots) else None,
+            
+            # player.update(
+            #     damage_cooldown = player["damage_cooldown"] - 1
+            # ) if player["damage_cooldown"] > 0 else None,
+
+            # Update cooldowns
+            update_cooldowns(player),
+            
+            # Player collides with robot
+            player.update(
+                health = player['health'] - 0.2,
+                damage_cooldown = 120,
+            ) if
+                player["damage_cooldown"] == 0
+                and any(
+                    has_collision(player, robot) 
+                for robot in robots) else None,
+            
+
             # Fire weapon
-            weapons[player["weapon"]](
-                player['x'],
-                player['y'],
-                player['dx'],
-                player['dy'],
+            (
+                weapons[player["weapon"]](
+                    player['x'],
+                    player['y'],
+                    player['dx'],
+                    player['dy'],
+                    ), 
+                player.update(
+                    weapon_cooldown = weapon_cooldowns[player["weapon"]]
+                )
             )
-                if is_key_down(KeyboardKey.KEY_SPACE) else None,
+                if is_key_down(KeyboardKey.KEY_SPACE) 
+                    and player["weapon_cooldown"] == 0
+                else None,
     
             # Update bullets
             globals().update(bullets = [
@@ -122,6 +175,13 @@
                 player["w"],
                 player["h"],
                 RED,
+            ),
+            draw_rectangle(
+                int(0 + window_w * 0.1), 
+                int(0 + window_h * 0.9),
+                int(player["health"] * healthbar_w),
+                healthbar_h,
+                BLUE,
             ),
             end_drawing(),
         ) for _ in cycle([1]))
